f4bcdfaacdfe46bc11b1d2aed9412c5f5e3d9151
Sam Phipps <samuel.v.phipps@gmail.com>
2022-12-01 13:45:28 -0600

    migration creation

--- Models/PetOwner.cs
+++ Models/PetOwner.cs
@@ -5,5 +5,22 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace pet_hotel
 {
-    public class PetOwner {}
+    public class PetOwner 
+    {
+        //primary key
+        public int id {get; set;}
+
+        //email
+        [Required]
+        public string emailAddress {get; set;}
+
+        //name
+        [Required]
+        public string name {get; set;}
+
+        //pet count
+        public int petCount {get; set;}
+
+
+    }
 }
