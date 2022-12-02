bcb5448fc7e4cb2dab036dc152d7e42544995a3f
Sam Phipps <samuel.v.phipps@gmail.com>
2022-12-02 11:29:03 -0600

    new migrations

---
+++ Migrations/20221202172801_CreatePetOwner2Table.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnetbakery.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatePetOwner2Table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
